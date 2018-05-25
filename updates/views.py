4c68f95b80b2d4a188f153aaf23fdaac50135765
System Administrator <root@Noorjahans-MacBook-Pro.local>
2018-05-26 05:04:10 +0600

    4 - A JSON Response - Completed

--- updates/views.py
+++ updates/views.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .models import Update
 
-# Create your views here.
+#def detial_view(request):
+    #return render(request, template_name, context_in_dict_form) # return JSON data  --> JS Object Notion
+    # example
+        # return render(request, "detail_view.html", {"some_key": "some_value", "some_other_key": "some_other_value"})
+            # the context gets converted to JSON data
+    #return HttpResponse(get_template("template_path").render({},request)) --> does the same
+
+def detail_view(request):
+
+    data = {
+        "count": 1000,
+        "content": "Some new content",
+        "boolean": True
+    }
+    return JsonResponse(data)
